38e7ead5198c6995bfae0c11aa53e3e5a4d3ac2d
samic++ <samichentouf+git2@gmail.com>
2025-09-07 23:01:11 +0200

    sync with static-serve

---
+++ link-executables.sh
@@ -0,0 +1,5 @@
+ln -s ./static-serve/bin/x86_64-unknown-linux-gnu ./server-x86_64
+ln -s ./static-serve/bin/aarch64-unknown-linux-gnu ./server-aarch64
+ln -s ./static-serve/bin/x86_64-pc-windows-gnu.exe ./server-x86_64-gnu.exe
+ln -s ./static-serve/bin/x86_64-pc-windows-gnu.exe ./server-x86_64-msvc.exe
+
