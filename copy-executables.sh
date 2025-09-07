b35a66c38ae0bebe2d4312bb22659dd33b7afcab
samic++ <samichentouf+git2@gmail.com>
2025-09-07 23:19:09 +0200

    added certificates for localhost

--- copy-executables.sh
+++ copy-executables.sh
@@ -1,4 +1,4 @@
 cp ./static-serve/bin/x86_64-unknown-linux-gnu ./server-x86_64
 cp ./static-serve/bin/aarch64-unknown-linux-gnu ./server-aarch64
 cp ./static-serve/bin/x86_64-pc-windows-gnu.exe ./server-x86_64-gnu.exe
-cp ./static-serve/bin/x86_64-pc-windows-gnu.exe ./server-x86_64-msvc.exe
+cp ./static-serve/bin/x86_64-pc-windows-msvc.exe ./server-x86_64-msvc.exe
